7f4bc9df52e8fc75ce162f34bcd1ff91a2687394
baptiste95350 <56865184+baptiste95350@users.noreply.github.com>
2021-06-02 09:46:47 +0200

    Add files via upload

---
+++ projet-responsive-design/assets/js/main.js
@@ -0,0 +1,22 @@
+// var burgerMenu = document.querySelector('.burger__menu');
+// var headerMenu = document.querySelector(".menu");
+// var headerOverlay = document.querySelector('.menu__overlay');
+// var body = document.querySelector('body');
+
+// console.log(burgerMenu, 'burger menu');
+
+
+// function toggleBurgerMenu(burger, menu, overlay) {
+//     burger.classList.toggle('active');
+//     menu.classList.toggle('active');
+//     overlay.classList.toggle('active');
+//     body.classList.toggle('stop-scroll');
+// }
+
+// burgerMenu.onclick = function () {
+//     toggleBurgerMenu(burgerMenu, headerMenu, headerOverlay);
+// }
+
+// headerOverlay.onclick = function () {
+//     toggleBurgerMenu(burgerMenu, headerMenu, headerOverlay);
+// }
